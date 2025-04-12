706c4e11ba15769ebb31c2b3ca14ac95c5a3ab70
morrisworks <morrischangetheworld@gmail.com>
2025-04-12 13:41:56 +0900

    userモデルのバリデーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :orders
+  has_many :items
+
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
