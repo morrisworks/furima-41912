fa850a2ec3a1a759752b7acf28fcde4ecd6944ad
morrisworks <morrischangetheworld@gmail.com>
2025-04-12 18:32:46 +0900

    fakerJapaneseの導入とusers.rbの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,24 @@
 FactoryBot.define do
+  password_generator = -> {
+    loop do
+      password = Faker::Internet.password(min_length: 6)
+      # 英字と数字が両方含まれているかを正規表現でチェック
+      if password.match(/(?=.*[a-zA-Z])(?=.*[0-9])/)
+        break password
+      end
+    end
+  }
+
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'example0'}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.email}
+    password              {password_generator.call}
     password_confirmation {password}
-    last_name             {'姓'}
-    first_name            {'名'}
-    last_name_kana        {'セイ'}
-    first_name_kana       {'メイ'}
-    birth_date            {Date.new(2000, 1, 1)}
+
+    last_name             {Faker::Japanese::Name.last_name}
+    first_name            {Faker::Japanese::Name.first_name}
+    last_name_kana        {Faker::Japanese::Name.last_name.yomi}
+    first_name_kana       {Faker::Japanese::Name.first_name.yomi}
+    birth_date            {Faker::Date.between(from: 100.years.ago, to: Date.today)}
   end
 end
