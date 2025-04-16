16a59701af5ead0a3bfda5f57be8711c6c172928
morrisworks <morrischangetheworld@gmail.com>
2025-04-16 17:05:31 +0900

    金額のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
   belongs_to :user
 
   # 空の投稿を保存できないようにする
-  validates :item_name, :item_info, :item_image, presence: true
+  validates :item_name, :item_info, :item_image, :item_price, presence: true
 
   # 各選択系（ActiveHash）カラムの選択が「---（id:1）」の時は保存できないようにする
   validates :prefecture_id,               numericality: { other_than: 1 , message: "can't be blank"}
@@ -18,5 +18,10 @@ class Item < ApplicationRecord
   validates :item_sales_status_id,       numericality: { other_than: 1 , message: "can't be blank"}
   validates :item_shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :item_scheduled_delivery_id,  numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :item_price, 
+    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'は300〜9,999,999円の間で、半角で入力してください' },
+    format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
+
 
 end
