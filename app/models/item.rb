51dd0b360c1f11fd8ee45d4fd200057ed69cc0f1
morrisworks <morrischangetheworld@gmail.com>
2025-04-16 12:48:51 +0900

    ActiveHushのアソシエーション、バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :item_category
+  belongs_to :item_sales_status
+  belongs_to :item_shipping_fee_status
+  belongs_to :item_scheduled_delivery
+
+  # 空の投稿を保存できないようにする
+  validates :title, :text, presence: true
+
+  # 各選択系（ActiveHash）カラムの選択が「---（id:1）」の時は保存できないようにする
+  validates :prefecture_id,               numericality: { other_than: 1 , message: "can't be blank"}
+  validates :items_category_id,           numericality: { other_than: 1 , message: "can't be blank"}
+  validates :items_sales_status_id,       numericality: { other_than: 1 , message: "can't be blank"}
+  validates :items_shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_scheduled_delivery_id,  numericality: { other_than: 1 , message: "can't be blank"}
+
 end
