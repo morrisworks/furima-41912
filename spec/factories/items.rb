8c9fa2752dbe6459599db210aa16817730e4a238
morrisworks <morrischangetheworld@gmail.com>
2025-04-18 18:48:57 +0900

    修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -12,7 +12,7 @@ FactoryBot.define do
     association :user
 
     after(:build) do |item|
-      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      item.item_image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
 
   end
