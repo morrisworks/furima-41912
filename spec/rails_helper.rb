ad87da07b9b370337f53cbf4b61388b21721220c
morrisworks <morrischangetheworld@gmail.com>
2025-04-21 07:25:53 +0900

    itemモデル単体テスト実装

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 
-I18n.locale = :en
 
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -9,6 +8,7 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+I18n.locale = :en
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
