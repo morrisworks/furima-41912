4e571d5eb1ac69bcead7ed4be2a7144627bd9592
morrisworks <morrischangetheworld@gmail.com>
2025-04-24 17:21:03 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :move_to_index, except: [:index, :show]
+
 
   def index
     @items = Item.order("created_at DESC")
@@ -13,6 +15,19 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -36,5 +51,12 @@ class ItemsController < ApplicationController
       :item_scheduled_delivery_id,
       :item_price
     ).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user.id
+      redirect_to action: :index
+    end
   end
 end
